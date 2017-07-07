02abd7726c7a10fcdbd4ddf6be407fc0b8d66cc4
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-07 12:15:43 -0400

    show pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
 		url: 'http://pokeapi.co/api/v2/pokemon/',
 		type: 'GET',
 		dataType: 'JSON',
-		data: {'limit': '811'},
+		data: {'limit': '20'},
 	})
 	.done(function(respuesta){
 		console.log(respuesta)
+		mostrarPokemon(respuesta.results)
 		console.log("succes");
 	})
 	.fail(function(){
@@ -15,4 +16,14 @@ $(document).ready(function(){
 	.always(function(){
 		console.log("complete");
 	})
+
+	var mostrarPokemon = function(data){
+
+	    data.forEach(function(e){
+	    	var pokeName = e.name;
+	    	$("#pokepoke").append("<div class='thepoke'><img class='pokeImg' src='http://img.pokemondb.net/artwork/"+ pokeName + ".jpg'><span class='pname'>"+ pokeName +"</span></div> ");
+	    })
+	};
+
+	
 });
