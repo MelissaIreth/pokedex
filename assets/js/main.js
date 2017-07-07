96f99036b7c4f09888269d01f827dc49fcaea141
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-07 10:15:10 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$.ajax({
+		url: 'http://pokeapi.co/api/v2/pokemon/',
+		type: 'GET',
+		dataType: 'JSON',
+		data: {'limit': '811'},
+	})
+	.done(function(respuesta){
+		console.log(respuesta)
+		console.log("succes");
+	})
+	.fail(function(){
+		console.log("error");
+	})
+	.always(function(){
+		console.log("complete");
+	})
+});
